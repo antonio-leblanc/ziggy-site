b3791e3460d8cb18b0d3a7a47ca45e6912583c55
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-10-19 21:05:01 -0300

    Auto-generated commit [ci skip]

--- website/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  mail_api: 'https://ziggycomidanatural.com.br/ziggy-server/mail.php'
-};
--- website/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  mail_api: 'https://ziggycomidanatural.com.br/ziggy-server/mail.php'
-};
